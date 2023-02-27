a24ece66a9970071ea92c459ec87fe853127b407
Declan Cross <crossdecl@ecs.vuw.ac.nz>
2023-02-27 15:39:40 +1300

    initial commit

---
+++ upgrade/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        hi
+
+
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
